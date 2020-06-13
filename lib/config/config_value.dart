2b742784e350d1980577e9c07ba26741583bedb8
Yogi <yogiektambakboyo@gmail.com>
2020-06-13 14:00:44 +0700

    btn fetch recipes done

--- lib/config/config_value.dart
+++ lib/config/config_value.dart
@@ -1,2 +1,3 @@
 String base_url = "https://lb7u7svcm5.execute-api.ap-southeast-1.amazonaws.com/dev";
+String base_url_pure = "lb7u7svcm5.execute-api.ap-southeast-1.amazonaws.com";
 String app_name = "Yuk Masak";
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:tech_task/config/config_value.dart';
 import 'package:tech_task/models/modelIngredients.dart';
+import 'package:tech_task/models/modelRecipes.dart';
 
 
 void main() {
@@ -40,6 +42,27 @@ Future<List<modelIngredients>> getIngredients() async {
 }
 
 
+Future<List<modelRecipes>> getRecipes(String Ingredients) async {
+  try{
+    var queryParameters = {
+      'ingredients': Ingredients,
+    };
+    var url = Uri.https(base_url_pure,'/dev/recipes',queryParameters);
+    var response = await http.get(url);
+    if(response.statusCode == 200) {
+      print(jsonDecode(response.body));
+      List<dynamic> data = jsonDecode(response.body);
+      var rest = data as List;
+      var result = rest.map<modelRecipes>((json) => modelRecipes.fromJson(json)).toList();
+      return result;
+    } else {
+      throw Exception('Something error');
+    }
+  } catch(e) {
+    throw Exception(e);
+  }
+}
+
 class MainScreen extends StatefulWidget{
   MainScreen({Key key}) : super(key:key);
 
@@ -53,10 +76,13 @@ class MainScreen extends StatefulWidget{
 class _MainScreenState extends State<MainScreen> {
   Future<List<modelIngredients>> futureIngredients;
   DateTime dateSelected = DateTime.now();
-  final f = new DateFormat('yyyy-MM-dd');
+  final dateFormat = new DateFormat('yyyy-MM-dd');
   List<modelIngredients> listAll = [];
   List<modelIngredients> listFiltered = [];
+  int ingredientCounter = 0;
 
+
+  Future<List<modelRecipes>> futureRecipes;
 
   @override
   void initState() {
@@ -68,6 +94,36 @@ class _MainScreenState extends State<MainScreen> {
     setState(() {
       dateSelected = date;
     });
+  }
+
+  void countIngredients(){
+    setState(() {
+      ingredientCounter=0;
+      for(int i=0;i<listFiltered.length;i++){
+        if(listFiltered[i].ischoice){
+          ingredientCounter++;
+        }
+      }
+    });
+  }
+
+  void resetIngredientsList(){
+    setState(() {
+      for(int i=0;i<listFiltered.length;i++){
+        listFiltered[i].ischoice = false;
+      }
+    });
+    countIngredients();
+  }
+
+  String resultIngredients(){
+    List<String> arrResult = [];
+    for(int i=0;i<listFiltered.length;i++){
+      if(listFiltered[i].ischoice){
+        arrResult.add(listFiltered[i].title);
+      }
+    }
+    return arrResult.join(",");
   }
 
   ListTile _tile(String title, String subtitle, bool ischeck,int idx) => ListTile(
@@ -91,8 +147,9 @@ class _MainScreenState extends State<MainScreen> {
         listFiltered[idx].ischoice = true;
       }
       print(title);
+      countIngredients();
       setState(() {
-        
+
       });
     },
   );
@@ -119,7 +176,7 @@ class _MainScreenState extends State<MainScreen> {
             Container(
               margin: const EdgeInsets.only(top: 10),
               child: new Text(
-                f.format(dateSelected),
+                dateFormat.format(dateSelected),
                 style: new TextStyle(
                   fontSize: 25.0,
                   color: new Color(0xFF8B1122),
@@ -142,6 +199,7 @@ class _MainScreenState extends State<MainScreen> {
                         print('change $date in time zone ' + date.timeZoneOffset.inHours.toString());
                       }, onConfirm: (date) {
                         print('confirm $date');
+                        resetIngredientsList();
                         setDate(date);
                       }, currentTime: DateTime.now(), locale: LocaleType.en);
                 },
@@ -149,6 +207,40 @@ class _MainScreenState extends State<MainScreen> {
                   'Click here for change Date',
                   style: TextStyle(color: Colors.blue),
                 )),
+            RaisedButton(
+              child: new Text("Get Recipes ( $ingredientCounter )"),
+              onPressed: (){
+                if(ingredientCounter>0){
+                  print(resultIngredients());
+                  futureRecipes = getRecipes(resultIngredients());
+                  FutureBuilder<List<modelRecipes>>(
+                    future: futureRecipes,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        if (snapshot == null) {
+                          print("Data Kosong");
+                        }
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        print("Error");
+                        print(snapshot.error.toString());
+                      }
+                      return CircularProgressIndicator();
+                    }
+                  );
+                }else{
+                  Fluttertoast.showToast(
+                      msg: "Please pick at least 1 ingredient to get recipes",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 16.0
+                  );
+                }
+              },
+            ),
             Expanded(child: Container(
               alignment: Alignment.center,
               padding: const EdgeInsets.all(8.0),
